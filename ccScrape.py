9662295c99f4298d86ddbf74fc1171b8a2965987
Hannesnt <111780097+Hannesnt@users.noreply.github.com>
2024-09-10 10:09:20 +0200

    Update ccScrape.py

--- ccScrape.py
+++ ccScrape.py
@@ -6,7 +6,7 @@ def ccScrape(browser, merchant):
     url = f"https://api.instore.bambora.com/api/merchants/{merchant}?embed=aggregations&embed=hostconfiguration&embed=services&embed=history&embed=licensesummary"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-        'Authorization': "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImNjLWxvZ2lzdGljcyIsIlVzZXIiOiJoYW5uZXMubmlsc3NvbnRlbmduYXNAd29ybGRsaW5lLmNvbSIsIkFjY291bnRJZCI6MTY5LCJBY2NvdW50IjoiY2MtbG9naXN0aWNzIiwibmJmIjoxNzIwNjA4MTg5LCJleHAiOjE3MjA2NDQxODksImlhdCI6MTcyMDYwODE4OSwiaXNzIjoiSW5zdG9yZUFQSSIsImF1ZCI6Ikluc3RvcmVBdWRpZW5jZSJ9.ot5JpC-Ivph8CUu7SmpJGDN4fqqjM_WxRNIa6ytVG34_GOYJiLNTpeSy1a8qaBmYTFhq306kUGPxIDL_UGzvVQJLNY6s8l8liRNUkRS4aerebIcweIL0EYcdljkFbor8ST-JglJ5mEWm-7_HmZT_sa1t1v4gSnw9vuvqW_uq4KZY5ZX49UJOGFbb_Gx2UOlL6v8IP3M3zEhgvGGOqqXmt4zUpxl4B8VsfXcaXLP7W2MXm0tzI1YsYDZTh9BqZ3TcoG3LpV-H9ym2kkgHHa4lsl4uoOvT11Oy3F4XJnzRWZU9ap4s-pDzshYx3CwxGigxW1V1UB-607UuomiucGKhzg"
+        'Authorization': f"Bearer {BEARER_TOKEN}"
     }
 
     response = requests.get(url, headers=headers)
